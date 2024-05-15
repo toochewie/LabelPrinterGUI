e06b8706bb916d1ab79d656326fe06b8b716e847
Chewie610 <105380641+Chewie610@users.noreply.github.com>
2024-05-14 21:05:29 -0400

    Initial Commit
    Backing up code to Github.
    Button.hta: First version of code, simple GUI with printing
    hold.hta: Adds holding to enter submenu with options
    option.hta: Enables printing logic for multiple options
    Chinese Project: Creating Chinese text as a graphic to add to the labels. Could not get fonts to work on the printer.

---
+++ print.bat
@@ -0,0 +1,12 @@
+@echo off
+:: Batch script to print ZPL file
+echo %1
+
+:: Print ZPL file to the default printer
+COPY %1 \\X380-Yoga\LoveBao
+
+::Delete ZPL file
+del %1
+
+:: End of script
+exit
